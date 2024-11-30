73e9399adb4b1be1b676a1e146251dbc5fd12532
Vadim Manannikov <manannikov.vv@gazprom-neft.ru>
2024-11-30 16:47:48 +0300

    Add DesignController

--- src/main/java/tacos/Ingredient.java
+++ src/main/java/tacos/Ingredient.java
@@ -6,7 +6,7 @@ import lombok.Data;
 public class Ingredient {
     private final String id;
     private final String name;
-    private final String type;
+    private final Type type;
 
     public enum Type {
         WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
