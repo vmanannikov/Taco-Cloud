692eb2477e4a614abd5fc423eb21f356a4a0b838
Vadim Manannikov <manannikov.vv@gazprom-neft.ru>
2024-12-01 01:09:43 +0300

    add validation to Taco and TacoOrder class

--- src/main/java/tacos/TacoOrder.java
+++ src/main/java/tacos/TacoOrder.java
@@ -1,6 +1,10 @@
 package tacos;
 
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.validator.constraints.CreditCardNumber;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,13 +12,29 @@ import java.util.List;
 @Data
 public class TacoOrder {
 
+    @NotBlank(message = "Delivery name is required")
     private String deliveryName;
+
+    @NotBlank(message = "Street is required")
     private String deliveryStreet;
+
+    @NotBlank(message = "City is required")
     private String deliveryCity;
+
+    @NotBlank(message = "State is required")
     private String deliveryState;
+
+    @NotBlank(message = "Zip is required")
     private String deliveryZip;
+
+    @CreditCardNumber(message = "Must be a valid credit card number")
     private String ccNumber;
+
+    @Pattern(regexp = "^(0[1-9]|1[0-2])([\\\\/])([2-9][0-9])$",
+             message = "Must be formatted MM/YY")
     private String ccExpiration;
+
+    @Digits(integer = 3, fraction = 0, message = "Must be 3 digits")
     private String ccCVV;
 
     private List<Taco> tacos = new ArrayList<>();
